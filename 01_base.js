8dc16c4845f7179a3f732a63af9b1f6522486ffa
Maxim Kazakov <kazakov.books@gmail.com>
2022-04-06 08:25:29 +0500

    Added 01_base.js and copy app.js in 01_base.js

---
+++ 01_base.js
@@ -0,0 +1,30 @@
+// устаревший вариант записи переменных
+//var name='Maxim'
+
+//пользоваться так
+//const myName='Maxim'  // тип string
+//let age=42          // тип number, т.к. JS не строго типизированный значение переменых может меняться в процессе выполнения программы.
+
+//console.log(myName,age) // в данном случае myName с типом string, age с типом number
+//console.log(myName+age) // обе переменные с типом string 
+
+//const lastName = prompt('Введите свою фамилию') // выводит на экран всплывающее окно
+//alert(myName + ' ' + lastName)
+
+//let X // undefined
+//const num=42 //number
+//const str = 'Kazakov' //string
+//null // null
+//const bool = true //boolean 
+//
+// МАССИВЫ
+
+const cars=['Mazda', 'BMW', 'Mercedes', 'Porshe']
+//for (let i=0; i<cars.length; i++){
+//    const car=cars[i]
+//    console.log(car)
+//}
+
+for (let car of cars){
+    console.log(car)
+}
